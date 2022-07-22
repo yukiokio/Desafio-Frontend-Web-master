e2b2b4e6ea8fbed221b7811342a362653a7435be
Fabio Matsumoto <hide.fabio123@gmail.com>
2022-07-22 18:17:36 -0300

    build: fix storybook components path

--- src/storybook/BodyMedium.stories.tsx
+++
@@ -1,9 +0,0 @@
-import BodyMedium from "../components/Typography/BodyMedium";
-
-export default {
-  title: "BodyMedium",
-  component: BodyMedium,
-};
-
-export const Primary = () => <BodyMedium>BodyMedium</BodyMedium>;
-export const Secondary = () => <BodyMedium bold>BodyMedium bold</BodyMedium>;
--- src/storybook/Input.stories.tsx
+++
@@ -1,8 +0,0 @@
-import Input from "../components/Forms/Input";
-
-export default {
-  title: "Input",
-  component: Input,
-};
-
-export const Primary = () => <Input>Input</Input>;
